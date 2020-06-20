b3d54f030cd2b2db64101012e704e7bac3df32ab
A Rohl <april.rohl2015@gmail.com>
2020-06-19 23:15:14 -0700

    Updates

---
+++ precache-manifest.0ac09767c91037b94e5ba554381fd836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b8811b1d172f3f3208f8423967833b",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "77a436c72a4b09f1739b",
+    "url": "/clickygame/static/css/main.ed16ba16.chunk.css"
+  },
+  {
+    "revision": "eefa867fd1b7e591c7f8",
+    "url": "/clickygame/static/js/2.f7bb8728.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.f7bb8728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a436c72a4b09f1739b",
+    "url": "/clickygame/static/js/main.888b573c.chunk.js"
+  },
+  {
+    "revision": "7122649522b9616ddb97",
+    "url": "/clickygame/static/js/runtime-main.71d67497.js"
+  }
+]);
